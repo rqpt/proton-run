0197a628f3207b3aa3c148f505dfd55a898c0b37
Ernst <ernst@cubezoo.joburg>
2023-08-31 20:01:27 +0200

    feat: Add support for running Steam games in proton-run.py

--- proton-run.py
+++ proton-run.py
@@ -2,6 +2,7 @@
 
 import os
 import requests
+import sys
 
 def get_steamid(game_name):
     url = f"https://api.steampowered.com/ISteamApps/GetAppList/v2/"
@@ -14,19 +15,17 @@ def get_steamid(game_name):
 
     return None
 
-executable = input("Which file would you like to run? ")
+executable = sys.argv[1]
 is_steam_game = input("Is this a steam game? (y/n) ")
 
 if is_steam_game.lower() == "y":
     game_name = input("Enter the game name: ")
     steamid = get_steamid(game_name)
+    print(f"The SteamId for this game is {steamid}")
     if steamid is None:
         print(f"Could not find Steam ID for {game_name}")
         exit(1)
 else:
     steamid = 480
 
-os.environ["SteamAppId"] = str(steamid)
-os.environ["STEAM_COMPAT_DATA_PATH"] = os.path.expanduser("~/proton/compatdata/")
-os.environ["STEAM_COMPAT_CLIENT_INSTALL_PATH"] = os.path.expanduser("~/proton/GE-Proton8-13/proton")
-os.system(f"waitforexitandrun {executable}")
+os.system(f"SteamAppId={steamid} STEAM_COMPAT_DATA_PATH=~/proton/compatdata/ STEAM_COMPAT_CLIENT_INSTALL_PATH=/ ~/proton/GE-Proton8-13/proton waitforexitandrun {executable}")
